17803cdb82f0d3851179a0215eb7a35714b070d8
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-07-24 16:41:29 -0300

    Update to use the new bulk-sale

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,6 +9,7 @@ export type Stock = Record<string, number>
 export type PaymentType = Schemas["PaymentType"]
 export type SaleRequest = Schemas["SaleRequest"]
 export type SalesRequests = SaleRequest[]
+export type BulkSaleRequest = Schemas["BulkSaleRequest"]
 export type RestockRequest = Schemas["RestockRequest"]
 export type WriteOffRequest = Schemas["WriteOffRequest"]
 
