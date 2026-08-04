640d8600c24ad1eef15f42fa711f9a2b03c39422
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-04 15:13:14 -0300

    Use fastapicloud url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import createClient from "openapi-fetch"
 import type { paths, components } from "./schema"
 
-const API_BASE = "http://192.168.0.164:8000"
+const API_BASE = "https://caad-erp.fastapicloud.dev"
 
 export const api = createClient<paths>({ baseUrl: API_BASE })
 export type Schemas = components["schemas"]
