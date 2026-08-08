d903e73d02871147a1b7d9d3a302273025ff8e39
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-07 23:02:51 -0300

    Update base url back to local

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import createClient from "openapi-fetch"
 import type { paths, components } from "./schema"
 
-const API_BASE = "https://caad-erp.fastapicloud.dev"
+const API_BASE = "http://0.0.0.0:8000"
 
 export const api = createClient<paths>({ baseUrl: API_BASE })
 export type Schemas = components["schemas"]
