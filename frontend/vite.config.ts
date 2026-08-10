d93465606ebe2c1cba6bde2be2026a8cfd622f3a
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-10 00:17:48 -0300

    Update mercado pago api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,24 +12,4 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
-    server: {
-        proxy: {
-            "/api-mp": {
-                target: "https://api.mercadopago.com",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api-mp/, ""),
-            },
-        },
-    },
-    preview: {
-        proxy: {
-            "/api-mp": {
-                target: "https://api.mercadopago.com",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api-mp/, ""),
-            },
-        },
-    },
 })
