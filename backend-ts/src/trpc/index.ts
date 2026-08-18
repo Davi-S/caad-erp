5eea76200b51b0c834dfc9b9d52b206295a4a841
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-18 19:59:29 -0300

    Base implmentation of trpc

---
+++ backend-ts/src/trpc/index.ts
@@ -0,0 +1,7 @@
+/**
+ * tRPC module barrel export file re-exporting context, primitives, and root AppRouter.
+ */
+
+export * from "./context.js"
+export * from "./trpc.js"
+export * from "./routers/_app.js"
