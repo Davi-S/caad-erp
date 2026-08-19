2e7e2c37ad7e75ec3584a78699cdbfef4bd19fdb
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-19 02:47:37 -0300

    Update imports

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,24 +1,24 @@
-import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from '../../../backend/src/trpc/index.js';
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
+import type { AppRouter } from "@backend/src/trpc/index.js"
 
-type RouterOutputs = inferRouterOutputs<AppRouter>;
-type RouterInputs = inferRouterInputs<AppRouter>;
+type RouterOutputs = inferRouterOutputs<AppRouter>
+type RouterInputs = inferRouterInputs<AppRouter>
 
-type NonVoid<T> = Exclude<T, void>;
+type NonVoid<T> = Exclude<T, void>
 
-export type Product = RouterOutputs['products']['list'][number];
-export type Products = Product[];
-export type ProductCreateRequest = NonVoid<RouterInputs['products']['add']>;
-export type ProductUpdateRequest = NonVoid<RouterInputs['products']['update']>['data'];
+export type Product = RouterOutputs["products"]["list"][number]
+export type Products = Product[]
+export type ProductCreateRequest = NonVoid<RouterInputs["products"]["add"]>
+export type ProductUpdateRequest = NonVoid<RouterInputs["products"]["update"]>["data"]
 
-export type Stock = RouterOutputs['reports']['inventory'];
-export type PaymentType = NonVoid<RouterInputs['transactions']['recordSale']>['paymentType'];
-export type SaleRequest = NonVoid<RouterInputs['transactions']['recordSale']>;
-export type SalesRequests = SaleRequest[];
-export type RestockRequest = NonVoid<RouterInputs['transactions']['recordRestock']>;
-export type WriteOffRequest = NonVoid<RouterInputs['transactions']['recordWriteOff']>;
+export type Stock = RouterOutputs["reports"]["inventory"]
+export type PaymentType = NonVoid<RouterInputs["transactions"]["recordSale"]>["paymentType"]
+export type SaleRequest = NonVoid<RouterInputs["transactions"]["recordSale"]>
+export type SalesRequests = SaleRequest[]
+export type RestockRequest = NonVoid<RouterInputs["transactions"]["recordRestock"]>
+export type WriteOffRequest = NonVoid<RouterInputs["transactions"]["recordWriteOff"]>
 
-export type Salesman = RouterOutputs['salesmen']['list'][number];
-export type Salesmen = Salesman[];
-export type SalesmanCreateRequest = NonVoid<RouterInputs['salesmen']['add']>;
-export type SalesmanUpdateRequest = NonVoid<RouterInputs['salesmen']['update']>['data'];
+export type Salesman = RouterOutputs["salesmen"]["list"][number]
+export type Salesmen = Salesman[]
+export type SalesmanCreateRequest = NonVoid<RouterInputs["salesmen"]["add"]>
+export type SalesmanUpdateRequest = NonVoid<RouterInputs["salesmen"]["update"]>["data"]
