bf65d56119d02612f69b548e1b601f6d6d82d7fb
Davi-S <67160664+Davi-S@users.noreply.github.com>
2026-08-19 07:04:54 -0300

    Add env and packages for mercado pago code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            "/api-mp": {
+            "/api/payments": {
                 target: "http://localhost:8000",
                 changeOrigin: true,
             },
